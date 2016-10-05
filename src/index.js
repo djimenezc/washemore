f34bec47e8edceebef56cf0289d6a5ddf05b6734
David <david.jimenez@logtrust.com>
2016-10-05 08:34:24 +0200

    WASHEMO-#53Construir app de catalogo de widgets
    - Using logger middleware

--- src/index.js
+++ src/index.js
@@ -8,16 +8,19 @@ import {applyMiddleware, createStore} from 'redux'
 import {Provider} from 'react-redux'
 
 const logger = () => (next) => (action) => {
-  console.log('action');
+  console.log('action fired', action.type, action.payload);
+  next(action);
 };
 
-const middleware = applyMiddleware();
+const middleware = applyMiddleware(logger);
 
-const store = createStore(reducers, 1, middleware);
+const initialState = {};
 
-// store.subscribe(() => {
-//   console.log('store changed', store.getState());
-// });
+const store = createStore(reducers, initialState, middleware);
+
+store.subscribe(() => {
+  console.log('store changed', store.getState());
+});
 
 store.dispatch({type: 'CHANGE_NAME', payload: 'david'});
 store.dispatch({type: 'CHANGE_NAME', payload: 'angel'});
